854771a1c4f6cf2b20df3ee7aae8a3fe56724b99
Olamide <olamide.adebisi18@gmail.com>
2022-07-06 11:07:12 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet_x10: prints the lowercase alphabet 10 times
+ */
+void print_alphabet_x10(void)
+{
+char a;
+char x;
+for (a = '0'; a < 10; a++)
+{
+for (x = 'a'; x <= 'z'; x++)
+_putchar(x);
+}	    
+_putchar('\n');
+}
