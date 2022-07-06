920ae4e95c4f780a15bf5e2e2c5365633ab92885
Olamide <olamide.adebisi18@gmail.com>
2022-07-06 09:38:34 -0700

    i sometimes suffer from insonmia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: always return 0
+ */
+int main(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+putchar(letter);
+putchar('\n');
+return (0);
+}
