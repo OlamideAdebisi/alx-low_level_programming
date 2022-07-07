b0317cfb408d377ec72bfbace1fe516bbc949420
Olamide <olamide.adebisi18@gmail.com>
2022-07-07 10:04:19 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
  * _islower - checks for a lower case alphabet
- * c - character to use
  * Return: return 1 if true and return 0 if false
  */
 int _islower(int c)
 {
+int c;
 if (c >= 'a' && c <= 'z')
 {
 return (1);
