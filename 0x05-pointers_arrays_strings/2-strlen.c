ca52c195a67256522624e9bcc94ce77feae6554a
Olamide <olamide.adebisi18@gmail.com>
2022-07-13 11:02:42 -0700

    This report, by its very lenght, defends itsef against the risk of being read

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _strlen - function that returns the length of a string
- * *s: lenth to be returned
+ * @s: lenth to be returned
  * Return: returns length of the string
  */
 int _strlen(char *s)
